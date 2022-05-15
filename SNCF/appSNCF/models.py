48d9e2edac3bd143fd8d79ea9888c18896b90ac8
olivier-goodenough <92865173+olivier-goodenough@users.noreply.github.com>
2022-05-15 23:16:03 +0200

    version problème importation css

--- SNCF/appSNCF/models.py
+++ SNCF/appSNCF/models.py
@@ -3,14 +3,16 @@ from django.db import models
 
 class train(models.Model):
     modele = models.CharField(max_length=100)
-    marque = models.CharField(max_length = 100)
     date_sortie = models.DateField(blank=True, null = True)
     nombre_en_stock= models.IntegerField(blank=False)
     resume = models.TextField(null = True, blank = True) # champs de type text long
+    marque = models.ForeignKey("marque", on_delete=models.CASCADE, default=None)
 
     def __str__(self):
             chaine = f"{self.modele} fabriqué par {self.marque} sortie le {self.date_sortie}"
             return chaine
+    def dico(self):
+        return {"modele": self.modele, "date_sortie": self.date_sortie,"nombre_en_stock":self.nombre_en_stock,"resume":self.resume,"marque": self.marque}
 
 
 class marque(models.Model):
@@ -19,3 +21,5 @@ class marque(models.Model):
     def __str__(self):
         chaine = f"{self.marque}"
         return chaine
+    def dico(self):
+        return {"marque": self.marque}
